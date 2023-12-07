29e679a034b2632625400b015df80585292c09ce
TopNik073 <borzovn169@gmail.com>
2023-12-07 22:59:03 +0200

    Start of DB

--- DB/DataBase.py
+++ DB/DataBase.py
@@ -1,37 +1,11 @@
 """Start of create DB!"""
+import sqlalchemy as sa
+from sqlalchemy import create_engine
 
-import sqlite3
+engine = create_engine("sqlite:///DataBase.db")
 
-DB_PATH = "app.db"
+connection = engine.connect()
 
-def connect_db():
-    """Config connection to DataBase"""
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    return conn
+metadata = sa.MetaData()
 
 
-def create_db():
-    """Create sheets inside the main DataBase"""
-
-    db = connect_db()
-    # db = sqlite3.connect(DB_PATH)
-    # db.row_factory = sqlite3.Row
-
-    db.cursor().executescript("""CREATE TABLE IF NOT EXISTS guilds (
-    id integer PRIMARY KEY,
-    count_members integer NOT NULL)
-    """)
-
-    db.cursor().executescript("""CREATE TABLE IF NOT EXISTS members(
-    id integer NOT NULL,
-    SCORES integer NOT NULL)
-    """)
-
-    db.commit()
-    db.close()
-
-# db = connect_db()
-# db = create_db(db)
-
-# C:\Users\andre\Documents\GitHub\Raff-3.0
