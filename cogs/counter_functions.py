25dc1671a3a1bd97ffbe27bfac27d00e31be56f9
andre <andreisafarov091@gmail.com>
2023-12-08 21:23:28 +0200

    Optimizing bot.py

--- cogs/counter_functions.py
+++ cogs/counter_functions.py
@@ -5,20 +5,6 @@ from json import load, dump
 FOLDER = getcwd()
 
 
-def count_messages_to_data(author_id) -> None:
-    """Counting every member's message"""
-    with(open(f"{FOLDER}/data/lb_messages_data.json", 'r', encoding='utf-8') as f):
-        data = load(f)
-
-    if str(author_id) not in data:
-        data[str(author_id)] = 1
-    else:
-        data[str(author_id)] += 1
-
-    with(open(f"{FOLDER}/data/lb_messages_data.json", 'w', encoding='utf-8') as f):
-        dump(data, f)
-
-
 def count_orders_counter() -> None:
     """Сounting the number of orders"""
     with (open(f"{FOLDER}/data/counters.json", "r", encoding="utf-8") as f):
@@ -41,16 +27,6 @@ def count_failed_gif_counter() -> None:
         dump(data, f)
 
 
-def count_lose_scores(scores: int):
-    with (open(f"{FOLDER}/data/counters.json", "r", encoding="utf-8") as f):
-        data = load(f)
-
-    data["LOSE_SCORES"] += scores
-
-    with (open(f"{FOLDER}/data/counters.json", "w", encoding="utf-8") as f):
-        dump(data, f)
-
-
 def count_number_of_events_counter() -> None:
     """Сounting the number of events"""
     with (open(f"{FOLDER}/data/counters.json", "r", encoding="utf-8") as f):
@@ -62,8 +38,18 @@ def count_number_of_events_counter() -> None:
         dump(data, f)
 
 
-def count_every_message(message) -> None:
-    """Counting every users' messages to data base"""
+def count_lose_scores(scores: int):
+    with (open(f"{FOLDER}/data/counters.json", "r", encoding="utf-8") as f):
+        data = load(f)
+
+    data["LOSE_SCORES"] += scores
+
+    with (open(f"{FOLDER}/data/counters.json", "w", encoding="utf-8") as f):
+        dump(data, f)
+
+
+async def count_every_message(message: disnake.Message) -> None:
+    """Counting every users' messages to database"""
     if not message.author.bot:
         author_id = message.author.id
         with(open(f"{FOLDER}/data/lb_messages_data.json", 'r', encoding='utf-8') as f):
@@ -78,7 +64,7 @@ def count_every_message(message) -> None:
             dump(data, f)
 
 
-def count_users_boosts(message) -> None:
+def count_users_boosts(message: disnake.Message) -> None:
     """Counting users' boost on the monitoring sites"""
     author_id = message.author.id
     with(open(f"{FOLDER}/data/counters.json", 'r', encoding='utf-8') as f):
--- cogs/on_message_functions.py
+++ cogs/on_message_functions.py
@@ -1,11 +1,68 @@
 import disnake
+from disnake.ext import commands
+from datetime import datetime
 from cogs import counter_functions
 
 
-async def reactions_thread_check(message: disnake.Message, settings_dict) -> None:
+async def moderation(message: disnake.Message, settings_dict: dict, bot: commands.Bot = None):
+    """Moderation functions"""
+    await gif_moderation(message, settings_dict["GIF"])
 
+
+async def gif_moderation(message: disnake.Message, settings_dict: dict):
+    """Gif moderation"""
+    if message.attachments:
+        if 'gif' in message.attachments[-1].url:
+            channel = message.channel
+
+            flag = False
+            skip_first_flag = False
+            async for msg in channel.history(limit=settings_dict['MESSAGES_FOR_GIF'] + 1):
+                if skip_first_flag:
+                    if msg.attachments:
+                        if 'gif' in msg.attachments[0].url:
+                            flag = True
+                            break
+                    if 'gif' in msg.content:
+                        flag = True
+                        break
+                skip_first_flag = True
+
+            if flag:
+                counter_functions.count_failed_gif_counter()
+
+                await message.reply(f"Гифки можно отправлять раз в {settings_dict['MESSAGES_FOR_GIF']} "
+                                    f"сообщений <a:A_heart1:993383076363239444>", delete_after=3)
+                await message.delete()
+
+    elif 'gif' in message.content:
+        channel = message.channel
+
+        flag = False
+        skip_first_flag = False
+        async for msg in channel.history(limit=settings_dict['MESSAGES_FOR_GIF'] + 1):
+            if skip_first_flag:
+                if msg.attachments:
+                    if 'gif' in msg.attachments[0].url:
+                        flag = True
+                        break
+                if 'gif' in msg.content:
+                    flag = True
+                    break
+            skip_first_flag = True
+
+        if flag:
+            counter_functions.count_failed_gif_counter()
+
+            await message.reply(f"Гифки можно отправлять раз в {settings_dict['MESSAGES_FOR_GIF']} "
+                                f"сообщений <a:A_heart1:993383076363239444>", delete_after=3)
+            await message.delete()
+
+
+async def reactions_thread_check(message: disnake.Message, settings_dict: dict) -> None:
+    """Adding reactions and(or) a thread to a message in the certain channels"""
     if str(message.channel.id) in settings_dict["CHANNELS"]:
-    
+
         channels = settings_dict["CHANNELS"]
         channels_thread = settings_dict["THREAD"]
         reactions = settings_dict["REACTIONS"]
@@ -15,25 +72,23 @@ async def reactions_thread_check(message: disnake.Message, settings_dict) -> Non
         print(message_channel_name)
 
         if message_channel_name in channels_thread:
-
             await message.create_thread(name="Комментарии")
-        
+
         for channel in matched_channels_to_reactions:
-            
+
             if message_channel_name == channel:
                 for reaction in matched_channels_to_reactions[channel]:
-
                     await message.add_reaction(reactions[reaction])
 
 
-async def boosts_check(message: disnake.Message, settings_dict) -> None:
-
+async def boosts_check(message: disnake.Message, settings_dict: dict) -> None:
+    """Checking if it's a boost (and counting number of them through separate function if so)"""
     if message.author.id in settings_dict["BOOST_BOTS"].values():
 
         if message.author.id == settings_dict["BOOST_BOTS"]["SD.C Monitoring"]:
 
             flag = True
-            skip_first_flag = False 
+            skip_first_flag = False
             async for msg in message.channel.history(limit=50):
                 if skip_first_flag:
                     if msg.type == disnake.MessageType.application_command and msg.interaction.name == "up":
@@ -49,26 +104,26 @@ async def boosts_check(message: disnake.Message, settings_dict) -> None:
 
         if message.author.id == settings_dict["BOOST_BOTS"]["DSMonitoring"]:
             if "Вы успешно лайкнули сервер." in message.embeds[-1].to_dict()["description"]:
-
                 counter_functions.count_users_boosts(message.interaction.user.id)
 
 
-if '/заказ' in message.content:
+async def order_command_check(bot: commands.Bot, message: disnake.Message, settings_dict: dict) -> None:
+    """A temporary way to use the slash command '/заказ'"""
+    if '/заказ ' in message.content:
 
-        channel = bot.get_channel(cfg.CHANNELS_SETTINGS["CHANNEL_RPBAR"])
-        barmen_role = "<@&829082636705595433>"
+        channel = bot.get_channel(settings_dict["CHANNEL"])
 
-        if message.channel.id != cfg.CHANNELS_SETTINGS["CHANNEL_RPBAR"]:
+        if message.channel.id != channel.id:
 
             await message.reply(
-                f"Эта команда может быть использована только в канале {channel.mention}!",
-                delete_after=5
+                f"Эта команда может быть использована только в канале {channel.mention}!", delete_after=5
             )
 
         else:
 
-            count_orders_counter()
+            counter_functions.count_orders_counter()
 
+            barmen_role = f"<@&{settings_dict['BARMEN_ROLE']}>"
             embed = disnake.Embed(
                 title="Новый заказ 📥",
                 description=f"{message.author.mention}\n{message.content.strip('/заказ ')}",
@@ -79,6 +134,6 @@ if '/заказ' in message.content:
 
             await message.reply(
                 f"Доброго времени суток {message.author.mention}! Бармен скоро подойдёт 🐥",
-                delete_after=20.0
+                delete_after=10
             )
             await channel.send(barmen_role, embed=embed)
