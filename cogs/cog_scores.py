796b7d893bd387190311457046bf6c10a775c73b
andre <andreisafarov091@gmail.com>
2023-12-09 21:58:12 +0200

    Optimizing cog_counters and cog_events

--- cogs/cog_scores.py
+++ cogs/cog_scores.py
@@ -9,12 +9,23 @@ from cogs import counter_functions
 FOLDER = getcwd()
 
 
+async def load_database() -> dict:
+    with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
+        data = load(f)
+    return data
+
+
+async def dump_database(data: dict) -> None:
+    with (open(f"{FOLDER}/data/users_data.json", "w", encoding="utf-8") as f):
+        dump(data, f)
+
+
 async def top_create_embed(bot: commands.Bot, embed_dict: dict):
     """Creating an embed of leaderboard of members by scores"""
     guild = bot.get_guild(cfg.GUILD_ID)
+    settings = cfg.COGS_SETTINGS["SCORES"]
 
-    with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-        data = load(f)
+    data = await load_database()
     sort_data = sorted(data.items(), key=lambda x: x[1], reverse=True)
     data = dict(sort_data)
 
@@ -26,9 +37,9 @@ async def top_create_embed(bot: commands.Bot, embed_dict: dict):
         data.pop(key)
 
     first_lvl_members, third_lvl_members, fifth_lvl_members = [], [], []
-    amount1 = cfg.COGS_SETTINGS["SCORES"]["AMOUNT_TO_FIRST_LVL"]
-    amount2 = cfg.COGS_SETTINGS["SCORES"]["AMOUNT_TO_THIRD_LVL"]
-    amount3 = cfg.COGS_SETTINGS["SCORES"]["AMOUNT_TO_FIFTH_LVL"]
+    amount1 = settings["AMOUNT_TO_FIRST_LVL"]
+    amount2 = settings["AMOUNT_TO_THIRD_LVL"]
+    amount3 = settings["AMOUNT_TO_FIFTH_LVL"]
 
     flag1, flag2, flag3 = False, False, False
     place = 0
@@ -41,42 +52,38 @@ async def top_create_embed(bot: commands.Bot, embed_dict: dict):
         elif amount2 <= value < amount3:
             flag2 = True
             third_lvl_members.append(key)
-        elif amount3 <= value:
+        elif value >= amount3:
             flag3 = True
             fifth_lvl_members.append(key)
         place += 1
 
     if flag1 or flag2 or flag3:
         embed_dict['description'] += "\n**Получат роли**"
-
-    index_of_field = 0
-    if flag1:
-        embed_dict['fields'].append({'name': '1-го уровня:', 'value': '', 'inline': True})
-        for key in first_lvl_members:
-            member = guild.get_member(int(key))
-            embed_dict['fields'][index_of_field]['value'] += f"{member.mention} "
-        index_of_field += 1
-
-    if flag2:
-        embed_dict['fields'].append({'name': '3-го уровня:', 'value': '', 'inline': True})
-        for key in third_lvl_members:
-            member = guild.get_member(int(key))
-            embed_dict['fields'][index_of_field]['value'] += f"{member.mention} "
-        index_of_field += 1
-
-    if flag3:
-        embed_dict['fields'].append({'name': '5-го уровня:', 'value': '', 'inline': True})
-        for key in fifth_lvl_members:
-            member = guild.get_member(int(key))
-            embed_dict['fields'][index_of_field]['value'] += f"{member.mention} "
-        index_of_field += 1
+        index_of_field = 0
+        if flag1:
+            embed_dict['fields'].append({'name': '1-го уровня:', 'value': '', 'inline': True})
+            for key in first_lvl_members:
+                member = guild.get_member(int(key))
+                embed_dict['fields'][index_of_field]['value'] += f"{member.mention} "
+            index_of_field += 1
+        if flag2:
+            embed_dict['fields'].append({'name': '3-го уровня:', 'value': '', 'inline': True})
+            for key in third_lvl_members:
+                member = guild.get_member(int(key))
+                embed_dict['fields'][index_of_field]['value'] += f"{member.mention} "
+            index_of_field += 1
+        if flag3:
+            embed_dict['fields'].append({'name': '5-го уровня:', 'value': '', 'inline': True})
+            for key in fifth_lvl_members:
+                member = guild.get_member(int(key))
+                embed_dict['fields'][index_of_field]['value'] += f"{member.mention} "
+            index_of_field += 1
 
     return embed_dict
 
 
 class ScoresOperations(commands.Cog):
-    """Where will be scores operations"""
-
+    """Where will be scores operations: /add_one, /remove_one, /add_any, /remove_any"""
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
@@ -84,52 +91,42 @@ class ScoresOperations(commands.Cog):
         description="Прибавить очки 1 участнику",
         default_member_permissions=disnake.Permissions(administrator=True)
     )
-    async def add_just_one(
-            self, inter: disnake.ApplicationCommandInteraction,
-            участник: disnake.Member, количество: int
+    async def add_one(
+            self, interaction: disnake.ApplicationCommandInteraction, участник: disnake.Member, количество: int
     ):
         """Adding to a member a certain amount of scores"""
         await counter_functions.count_added_scores(количество)
 
-        with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-            data = load(f)
-
+        data = await load_database()
         if str(участник.id) not in data:
             data[str(участник.id)] = количество
         else:
             data[str(участник.id)] += количество
+        await dump_database(data)
 
-        with (open(f"{FOLDER}/data/users_data.json", "w", encoding="utf-8") as f):
-            dump(data, f)
-
-        await inter.response.send_message(f"Теперь у {участник} {data[str(участник.id)]} оч.")
+        await interaction.response.send_message(f"Теперь у {участник} {data[str(участник.id)]} оч.")
 
     @commands.slash_command(
         description="Вычесть очки у 1 участника",
         default_member_permissions=disnake.Permissions(administrator=True)
     )
-    async def remove_just_one(
-            self, inter: disnake.ApplicationCommandInteraction,
-            участник: disnake.Member, количество: int
+    async def remove_one(
+            self, interaction: disnake.ApplicationCommandInteraction, участник: disnake.Member, количество: int
     ):
         """Removing from a member a certain amount of scores"""
         await counter_functions.count_removed_scores(количество)
 
-        with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-            data = load(f)
-
+        data = await load_database()
         if str(участник.id) not in data:
-            await inter.response.send_message("У участника 0 очков, куда меньше?...")
+            await interaction.response.send_message("У участника 0 очков, куда меньше?...")
         else:
             if количество >= data[str(участник.id)]:
                 data[str(участник.id)] = 0
             else:
                 data[str(участник.id)] -= количество
+        await dump_database(data)
 
-        with (open(f"{FOLDER}/data/users_data.json", "w", encoding="utf-8") as f):
-            dump(data, f)
-
-        await inter.response.send_message(f"Теперь у {участник} {data[str(участник.id)]} оч.")
+        await interaction.response.send_message(f"Теперь у {участник} {data[str(участник.id)]} оч.")
 
     @commands.slash_command(
         description="Вычесть очки у любого кол-ва участников (упомянуть через пробел)",
@@ -137,9 +134,7 @@ class ScoresOperations(commands.Cog):
     )
     async def remove_any(self, interaction: disnake.ApplicationCommandInteraction, участники: str, количество: int):
         """Removing from several members a certain amount of scores"""
-        with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-            data = load(f)
-
+        data = await load_database()
         members_list = участники.split()
         members_list_values = []
 
@@ -156,9 +151,7 @@ class ScoresOperations(commands.Cog):
                 else:
                     data[str(member_id)] -= количество
                     members_list_values.append(data[str(member_id)])
-
-        with (open(f"{FOLDER}/data/users_data.json", "w", encoding="utf-8") as f):
-            dump(data, f)
+        await dump_database(data)
 
         members_dict = dict(zip(members_list, members_list_values))
         embed = disnake.Embed(
@@ -178,9 +171,7 @@ class ScoresOperations(commands.Cog):
     )
     async def add_any(self, interaction: disnake.ApplicationCommandInteraction, участники: str, количество: int):
         """Adding to several members a certain amount of scores"""
-        with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-            data = load(f)
-
+        data = await load_database()
         members_list = участники.split()
         members_list_values = []
 
@@ -194,9 +185,7 @@ class ScoresOperations(commands.Cog):
             else:
                 data[str(member_id)] += количество
                 members_list_values.append(data[str(member_id)])
-
-        with (open(f"{FOLDER}/data/users_data.json", "w", encoding="utf-8") as f):
-            dump(data, f)
+        await dump_database(data)
 
         members_dict = dict(zip(members_list, members_list_values))
         embed = disnake.Embed(
@@ -215,31 +204,25 @@ class ScoresOperations(commands.Cog):
         default_member_permissions=disnake.Permissions(administrator=True)
     )
     async def set_just_one(
-            self,
-            inter: disnake.ApplicationCommandInteraction,
-            участник: disnake.Member, количество: int
+            self, interaction: disnake.ApplicationCommandInteraction, участник: disnake.Member, количество: int
     ):
         """Setting for a member a certain amount of scores"""
-        with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-            data = load(f)
-
+        data = await load_database()
         data[str(участник.id)] = количество
+        await dump_database(data)
 
-        with (open(f"{FOLDER}/data/users_data.json", "w", encoding="utf-8") as f):
-            dump(data, f)
-
-        await inter.response.send_message(f"У {участник} теперь {количество}")
+        await interaction.response.send_message(f"У {участник} теперь {количество}")
 
 
 class SpecialScoresCommands(commands.Cog):
+    """Special scores commands: /реп, /топ, /reset"""
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.slash_command(description="Показать кол-во очков у себя / участника")
     async def реп(self, interaction: disnake.ApplicationCommandInteraction, участник: disnake.Member = None):
         """Showing user's or a somebody's amount of scores"""
-        with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-            data = load(f)
+        data = await load_database()
 
         embed_dict = {
             'fields': [{'name': 'Очки'}],
@@ -254,7 +237,7 @@ class SpecialScoresCommands(commands.Cog):
                 embed_dict['thumbnail'] = 'https://i.postimg.cc/CMsM38p8/1.png'
             try:
                 embed_dict['fields'][0]['value'] = f"```{data[str(участник.id)]} оч.```"
-            except:
+            except KeyError:
                 embed_dict['fields'][0]['value'] = f'```0 оч.```'
         else:
             embed_dict['title'] = f'{interaction.author}'
@@ -264,7 +247,7 @@ class SpecialScoresCommands(commands.Cog):
                 embed_dict['thumbnail'] = 'https://i.postimg.cc/CMsM38p8/1.png'
             try:
                 embed_dict['fields'][0]['value'] = f"```{data[str(interaction.author.id)]} оч.```"
-            except:
+            except KeyError:
                 embed_dict['fields'][0]['value'] = f'```0 оч.```'
 
         await interaction.response.send_message(embed=disnake.Embed.from_dict(embed_dict))
@@ -295,23 +278,19 @@ class SpecialScoresCommands(commands.Cog):
         description="Сброс всех очков (пароль)",
         default_member_permissions=disnake.Permissions(administrator=True)
     )
-    async def reset(self, inter: disnake.ApplicationCommandInteraction, пароль: int):
+    async def reset(self, interaction: disnake.ApplicationCommandInteraction, пароль: int):
         """Resetting scores database and making backup"""
         if пароль == cfg.SETTINGS["PASSWORD"]:
-            with (open(f"{FOLDER}/data/users_data.json", "r", encoding="utf-8") as f):
-                data = load(f)
-
+            data = await load_database()
             with (open(f"{FOLDER}/data/backups/backup_{date.today()}.json", 'w', encoding="utf-8") as f):
                 dump(data, f)
+            await dump_database({})
 
-            with (open(f"{FOLDER}/data/users_data.json", "w", encoding="utf-8") as f):
-                dump({}, f)
-
-            await inter.response.send_message(f"База данных сброшена, бэкап создан `{date.today()}`")
+            await interaction.response.send_message(f"База данных сброшена, бэкап создан `{date.today()}`")
 
         else:
 
-            await inter.response.send_message("Неверный пароль... Ты вор, сука?", delete_after=30)
+            await interaction.response.send_message("Неверный пароль... Ты вор, сука?", delete_after=30)
 
 
 def setup(bot: commands.Bot):
