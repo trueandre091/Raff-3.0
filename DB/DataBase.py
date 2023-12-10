2db61144bca2b064cec248093d2d0612424891a1
TopNik073 <borzovn169@gmail.com>
2023-12-10 20:56:04 +0200

    Db update
    Update models and DataBase class

--- DB/DataBase.py
+++ DB/DataBase.py
@@ -1,53 +1,10 @@
 """Start of create DB!"""
-from sqlalchemy import create_engine, select, Column, Integer, String, ForeignKey, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, Session
-from JSONEnc import JsonEncoder
-from config_default import GUILD_CONFIG
+from typing import Union
 
-engine = create_engine("sqlite:///DataBase.db", echo=True)
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-Base = declarative_base()
-
-
-class Guild_User(Base):
-    __tablename__ = "guild_user"
-
-    id = Column(Integer, primary_key=True)
-    disc_id = Column(Integer, ForeignKey("users.disc_id"))
-    guild_id = Column(Integer, ForeignKey("guilds.guild_id"))
-    extra_info = Column(Text)
-
-    def __repr__(self) -> str:
-        return f"id={self.id!r}, disc_id={self.disc_id!r}, guild_id={self.guild_id!r}, extra_info={self.extra_info!r}"
-
-
-class Users(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(30), nullable=False)
-    disc_id = Column(Integer, nullable=False)
-    experience = Column(Integer, nullable=False, default=0)
-    scores = Column(Integer, nullable=False, default=10)
-    guilds = relationship("Guild_User", backref='user')
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, username={self.username!r}, disc_id={self.disc_id!r}, experience={self.experience!r}, scores={self.scores!r}, guilds={self.guilds!r}"
-
-
-class Guilds(Base):
-    __tablename__ = "guilds"
-
-    id = Column(Integer, primary_key=True)
-    guild_id = Column(Integer, nullable=False)
-    guild_name = Column(String(30), nullable=False)
-    count_of_members = Column(Integer, nullable=False)
-    guild_settings = Column(Text, nullable=False, default=lambda x: await JsonEncoder.code_to_json(GUILD_CONFIG))
-    users = relationship("Guild_User", backref="guild")
-
-    def __repr__(self) -> str:
-        return f"id={self.id!r}, guild_id={self.guild_id!r}, guild_name={self.guild_name!r}, count_of_members={self.count_of_members!r}, guild_settings={self.guild_settings!r}"
+from models import Users, Guilds
 
 
 class DataBase:
@@ -55,10 +12,16 @@ class DataBase:
 
     def __init__(self):
         try:
-            self.engine = create_engine("sqlite:///DataBase.db", echo=True)
-            self.session = Session(self.engine)
+            self.engine = create_async_engine("sqlite:///DataBase.db", echo=True)
+            self.session = self.get_session(self.engine)
         except:
             raise "Error creating an engine or connecting to database"
+
+    @staticmethod
+    def get_session(engine):
+        session = async_sessionmaker(engine)
+        async with session() as session:
+            return session
 
     async def get_guild(self, id=None, guild_id=None, guild_name=None):
         try:
@@ -129,7 +92,7 @@ class DataBase:
 
         return False
 
-    async def add_user(self, args: dict) -> [True, False]:
+    async def add_user(self, args: dict) -> Union[True, False]:
         data = args
         try:
             user = Users(username=data["username"], disc_id=data["disc_id"])
@@ -140,7 +103,7 @@ class DataBase:
 
         return False
 
-    async def add_guild(self, args) -> [True, False]:
+    async def add_guild(self, args) -> Union[True, False]:
         data = args
         try:
             guild = Guilds(guild_id=data["guild_id"], guild_name=data["guild_name"],
@@ -153,12 +116,8 @@ class DataBase:
 
         return False
 
-    async def update_user(self, args) -> [True, False]:
+    async def update_user(self, args) -> Union[True, False]:
         pass
 
-    async def update_guild(self, args) -> [True, False]:
+    async def update_guild(self, args) -> Union[True, False]:
         pass
-
-
-if __name__ == "__main__":
-    Base.metadata.create_all(engine)
---
+++ DB/models.py
@@ -0,0 +1,73 @@
+from typing import Annotated
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from JSONEnc import JsonEncoder
+from config_default import GUILD_CONFIG
+
+intid = Annotated[int, mapped_column(primary_key=True)]
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Guild_User(Base):
+    __tablename__ = "guild_user"
+
+    id: Mapped[intid]
+    disc_id: Mapped[int] = mapped_column(ForeignKey("users.c.disc_id"))
+    guild_id: Mapped[int] = mapped_column(ForeignKey("guilds.c.guild_id"))
+
+    # id = Column(Integer, primary_key=True)
+    # disc_id = Column(Integer, ForeignKey("users.disc_id"))
+    # guild_id = Column(Integer, ForeignKey("guilds.guild_id"))
+    # extra_info = Column(Text)
+
+    def __repr__(self) -> str:
+        return f"id={self.id!r}, disc_id={self.disc_id!r}, guild_id={self.guild_id!r}"
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[intid]
+    username: Mapped[str]
+    disc_id: Mapped[int]
+    experience: Mapped[int] = mapped_column(default=0)
+    scores: Mapped[int] = mapped_column(default=10)
+    guilds: Mapped["Guild_User"] = relationship(backref='user')
+
+    # id = Column(Integer, primary_key=True)
+    # username = Column(String(30), nullable=False)
+    # disc_id = Column(Integer, nullable=False)
+    # experience = Column(Integer, nullable=False, default=0)
+    # scores = Column(Integer, nullable=False, default=10)
+    # guilds = relationship("Guild_User", backref='user')
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r}, disc_id={self.disc_id!r}, experience={self.experience!r}, scores={self.scores!r}, guilds={self.guilds!r}"
+
+
+class Guilds(Base):
+    __tablename__ = "guilds"
+
+    id: Mapped[intid]
+    guild_id: Mapped[int]
+    guild_name: Mapped[str]
+    count_of_members: Mapped[int] = mapped_column(nullable=True)
+    guild_sets: Mapped[str] = mapped_column(default=lambda x: await JsonEncoder.code_to_json(GUILD_CONFIG))
+
+    # id = Column(Integer, primary_key=True)
+    # guild_id = Column(Integer, nullable=False)
+    # guild_name = Column(String(30), nullable=False)
+    # count_of_members = Column(Integer, nullable=False)
+    # guild_settings = Column(Text, nullable=False, default=lambda x: await JsonEncoder.code_to_json(GUILD_CONFIG))
+    # users = relationship("Guild_User", backref="guild")
+
+    def __repr__(self) -> str:
+        return f"id={self.id!r}, guild_id={self.guild_id!r}, guild_name={self.guild_name!r}, count_of_members={self.count_of_members!r}, guild_settings={self.guild_sets!r}"
+
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///DataBase.db", echo=True)
+    Base.metadata.create_all(engine)
