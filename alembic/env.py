9c2a77cd094599e97a8067bd91f7d6880a9a114e
TopNik073 <borzovn169@gmail.com>
2023-12-19 23:51:55 +0200

    Alembic updates

--- alembic/env.py
+++ alembic/env.py
@@ -18,15 +18,17 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from DB.models import Base
+# from DB.models import Base
+from DB.models import *
 
 target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", "sqlite:///DB/DataBase.db")
+# config.set_main_option("sqlalchemy.url", "sqlite:///DB/DataBase.db")
 
 
 def run_migrations_offline() -> None:
