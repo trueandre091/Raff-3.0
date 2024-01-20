ef5bebba3a6852416dd8b927acd0e282720a0eac
andre <andreisafarov091@gmail.com>
2024-01-20 17:19:53 +0200

    new config

--- utils/new_config.py
+++ utils/new_config.py
@@ -1,55 +1,76 @@
 GUILD_CONFIG = {
     "GUILD_ID": None,
-    "TIMERS": {
-        "CHANNEL": None,
-        "MESSAGE": "Таймер",
-    },
-    "GENERAL_SETTINGS": {
+    "GENERAL": {
         "PASSWORD": 111111,
         "WELCOME": False,
         "FAREWELL": False,
         "MODERATION": {"GIF": False},
-        "AUTOUPDATE_MESSAGES": {"MESSAGES": False, "SCORES": False},
+        "AUTOUPDATE": {"MESSAGES": False, "SCORES": False},
         "NEAREST_EVENTS": False,
         "EXPERIENCE": False,
-        "GAMES": {"BLACKJACK": False, "ROULETTE": False},
+        "GAMES": {"ALL_GAMES": False, "BLACKJACK": False, "ROULETTE": False},
         "ORDERS": False,
         "REQUESTS": False,
         "SCORES": False,
-        "COUNTING_BOOSTS": False,
-        "ADDING_REACTIONS_THREADS": False,
-        "AUTO_ADDING_ROLES": False,
-        "AUTO_ADDING_SCORES_FOR_TIME_IN_VOICE_CHANNEL": False,
+        "BOOSTS": False,
+        "REACTIONS_THREADS": False,
+        "ROLES": False,
+        "EVENT_REWARDING": False,
     },
-    "WELCOME_SETTINGS": {
-        "CHANNEL": None,
-        "EMBED": {
-            # возможно использование переменных: {member.mention}, {member}
-            "TITLE": "Добро пожаловать на сервер!  💌",
-            # возможно использование переменных: {member.mention}, {member}
-            "DESCRIPTION": "{member.mention}, рады тебя видеть!\nПриходи на ближайшие мероприятия, если будет время <#1160687361269694505> 🌼",
-            "COLOR": 0x2B2D31,
+    ###########################################################
+    "COGS": {
+        "WELCOME": {
+            "CHANNEL": None,
+            "EMBED": {
+                "TITLE": "Добро пожаловать на сервер!  💌",
+                "DESCRIPTION": "{member.mention}, рады тебя видеть на нашем сервере!\n",
+                "BACKGROUND_IMAGE": "https://i.postimg.cc/25znZVnG/DALLE-2023-02-22-19-01-33-a-spring-flowery-background-1.png",
+                "AVATAR_IF_ERROR": "https://im.wampi.ru/2023/11/02/Bez_nazvania1_20211210115049.png",
+                "COLOR": 0x2B2D31,
+            },
         },
-        "AVATAR_IF_ERROR": "https://im.wampi.ru/2023/11/02/Bez_nazvania1_20211210115049.png",
-        "BACKGROUND_IMAGE": "https://i.postimg.cc/25znZVnG/DALLE-2023-02-22-19-01-33-a-spring-flowery-background-1.png",
-    },
-    "FAREWELL_SETTINGS": {
-        "CHANNEL": None,
-        # возможно использование переменных: {member.mention}, {member.nick}, {member.name}, {member}
-        "MESSAGE": "{member.mention} / {member.name} / {member.nick} ушёл.",
-    },
-    "ADDING_REACTIONS_THREADS_SETTINGS": {
-        # None: {"REACTIONS": [], "THREAD": False},
-    },
-    "MODERATION_SETTINGS": {"CHANNEL": [None], "GIF": {"MESSAGES_FOR_GIF": 10}},
-    "BOOSTS_COUNTING_SETTINGS": {
-        "BOOST_BOTS": {
-            "DSMonitoring": 575776004233232386,
-            "SD.C Monitoring": 464272403766444044,
+        "FAREWELL": {
+            "CHANNEL": None,
+            "MESSAGE": "{member.mention} / {member.name} / {member.nick} ушёл с сервера!",
         },
-        "REMINDER": 478321260481478677,
-    },
-    "COGS_SETTINGS": {
+        "REACTIONS_THREADS": {
+            # None: {"REACTIONS": [], "THREAD": False},
+        },
+        "MODERATION": {"CHANNELS": [], "GIF": {"DELAY": 10}},
+        "SCORES": {
+            "REWARDS": [
+                # {"ROLE": None, "AMOUNT": None}
+            ],
+        },
+        "EXPERIENCE": {
+            "REWARDS": [
+                # {"ROLE": None, "AMOUNT": None}
+            ],
+        },
+        "AUTOUPDATE": {
+            "CHANNEL": None,
+            "MESSAGES": {"LIMIT": 10},
+            "SCORES": {"LIMIT": 20},
+        },
+        "REQUESTS": {
+            "MESSAGE": {
+                "EMBED": {
+                    "TITLE": "Служба поддержки 📟",
+                    "DESCRIPTION": "При помощи кнопок к сообщению вы можете сделать запрос по любому поводу у администрации, нажав на соответствующую кнопку ниже",
+                    "COLOR": 0x2B2D31,
+                },
+                "CALLBACK": "Запрос успешно отправлен! <a:A_heart1:993383076363239444>\nЖдите ответ от администрации, он будет отправлен вам в личные сообщения",
+            },
+            "CHANNELS": {
+                "LOGS": None,
+                "REMINDER": None,
+            },
+        },
+        "NEAREST_EVENTS": {
+            "CHANNEL": None,
+            "CATEGORIES": ["Еженедельный ивент"],
+        },
+        "GAMES": {"CHANNELS": [], "ROULETTE": {"CHANCE": 60}},
         "SPECIAL": {
             "ROLES": {
                 # "BOOSTS": {
@@ -57,46 +78,26 @@ GUILD_CONFIG = {
                 #     "ROLES_GET": [None],
                 # }
             },
-            "EVENTS": {
-                "TIME": 600,
-                "CHANNELS": [None],
-                "ADMIN_CHANNEL": None,
+            "EVENT_REWARDING": {
+                "CHANNEL": None,
+                "TIME": 10,
+                "VOICES": [],
                 "ROLES": [
-                    {"ROLES_ID": [None], "SCORES": None},
-                    {"ROLES_ID": "everyone", "SCORES": 1},
+                    # {"ROLES": [], "SCORES": None},
+                    {"ROLES": "everyone", "SCORES": 1},
                 ],
             },
         },
-        "SCORES": {
-            "AMOUNT_TO_FIRST_LVL": 8,
-            "AMOUNT_TO_THIRD_LVL": 12,
-            "AMOUNT_TO_FIFTH_LVL": 16,
-        },
-        "GAMES": {"ROULETTE": {"CHANCE": 60}},
-        "AUTOUPDATE": {
-            "MESSAGES": {"PLACE_LIMIT": 10},
-            "SCORES": {"PLACE_LIMIT": 20},
-            "CHANNEL": None,
-        },
-        "NEAREST_EVENTS": {
-            "CHANNEL": None,
-            "CATEGORIES": ["Еженедельный ивент", "Особый ивент"],
-        },
-        "ORDERS": {"CHANNEL": None, "BARMEN_ROLE": None},
-        "REQUESTS": {
-            "BUTTONS_MESSAGE": {
-                "EMBED": {
-                    "TITLE": "Служба поддержки 📟",
-                    "DESCRIPTION": "При помощи кнопок к сообщению вы можете сделать запрос по любому поводу у администрации и оставить отзыв о качестве поддержки, нажав на соответствующие кнопки ниже",
-                    "COLOR": 0x2B2D31,
-                },
-                "CALLBACK": "Запрос успешно отправлен! <a:A_heart1:993383076363239444>\n`Ждите ответ от администрации, он будет отправлен вам в личные сообщения`",
+        "ORDERS": {"CHANNEL": None, "ROLE": None},
+        "BOOSTS_COUNTING": {
+            "BOTS": {
+                "DSMonitoring": 575776004233232386,
+                "SD.C Monitoring": 464272403766444044,
             },
-            "LOGS_MESSAGE": {"CHANNEL": None},
-            "ADMIN_CHANNEL": None,
+            "REMINDER": 478321260481478677,
         },
-        "EXPERIENCE": {"FACTOR": 5, "LEVELING": [{"ROLE": None, "AMOUNT": None}]},
     },
+    ###########################################################
     "COUNTERS": {
         "LOSE_SCORES": 0,
         "MESSAGES_PREVIOUS_BESTS": [],
